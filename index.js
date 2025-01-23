6af9ae3133ff98f96985e2adab0430712db68a45
AnnmariyaMa <2302895D@student.tp.edu.sg>
2025-01-23 16:19:38 +0800

    Completed the implementation of real-time monitoring and logging

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-
+const logger = require('./logger');
 
 const PORT = process.env.PORT || 5051;
 var startPage = "index.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
 const { addbeverage, viewBeverage } = require("./utils/beverageUtil");
 const { editbeverage } = require("./utils/updateUtil");
@@ -25,5 +28,7 @@ server = app.listen(PORT, function () {
         address.address == "::" ? "localhost" : address.address
     }:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
+    logger.info(`Demo project at: ${baseUrl}!`);
+    logger.error(`Example or error log`)
 });
 module.exports = { app, server };
